a7299a888f9adeb260e760bd52af9e588995803e
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2021-02-09 11:51:01 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {combineLatest, from, Observable, of} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'multi-upload';
+
+  uploads: any[] =[];
+  allPercentage:Observable<any>=of();
+  files:Observable<any>=of();
+
+  ngOnInit(): void {
+    this.files = this.fas.collection('files').valueChanges();
+  }
+
+  constructor(private fas:AngularFirestore,private storage:AngularFireStorage) {
+  }
+
+  importImages(event:any){
+    this.uploads = [];
+    const filelist = event.target.files;
+    const allPercentage: Observable<number>[] = [];
+
+    for(const file of filelist){
+      const path=`files/${file.name}`;
+      const ref =this.storage.ref(path);
+      const task=this.storage.upload(path,file);
+      const _percentage$= task.percentageChanges();
+
+      allPercentage.push(_percentage$ as Observable<number>);
+
+
+      // create composed object with different information. ADAPT THIS ACCORDING YOUR NEED
+      const uploadTrack = {
+        fileName: file.name,
+        percentage: _percentage$
+      }
+
+      this.uploads.push(uploadTrack);
+      const _t=task.then((f)=>{
+        return f.ref.getDownloadURL().then((url)=>{
+          return this.fas.collection('files').add({
+            name:f.metadata.name,
+            url:url
+          });
+
+        })
+      });
+      this.allPercentage = combineLatest(allPercentage)
+        .pipe(
+          map((percentages) =>{
+            let result= 0;
+            for(const percentage of percentages){
+              result = result + percentage;
+            }
+            return  result / percentages.length;
+          }),
+          tap(console.log)
+        )
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_QF3xIWXzU0Hbh8p1dSx8OcqTab2MoL4",
+  authDomain: "fire-course-c7496.firebaseapp.com",
+  databaseURL: "https://fire-course-c7496.firebaseio.com",
+  projectId: "fire-course-c7496",
+  storageBucket: "fire-course-c7496.appspot.com",
+  messagingSenderId: "864826074044",
+  appId: "1:864826074044:web:d4ae2c75e4263e6e1641b9",
+  measurementId: "G-RF4Z31SEPT"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +23,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
